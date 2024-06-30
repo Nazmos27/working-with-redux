69f213cd1c9e9484dc97aaba8f11bc274b5c6b91
Nazmos27 <nazmos789@gmail.com>
2024-06-30 19:58:13 +0600

    first counter made with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./redux/features/counterSlice";
+
 function App() {
+
+  const {count} = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
+  
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <div className="flex border-b-2 bg-slate-300 ">
-        <button className="px-4 bg-green-400 py-3 rounded-md text-white ">
+        <button onClick={() => dispatch(increment()) } className="px-4 bg-green-400 py-3 rounded-md text-white ">
           Increment
         </button>
-        <h1 className="text-3xl mx-10">0</h1>
-        <button className="px-4 bg-red-400 py-3 rounded-md text-white">
+        <h1 className="text-3xl mx-10">{count}</h1>
+        <button onClick={() => dispatch(decrement())} className="px-4 bg-red-400 py-3 rounded-md text-white">
           Decrement
         </button>
       </div>
